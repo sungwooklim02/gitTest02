9f83c069538460eb6e7a178b0812e98ffe07cef9
D7703_30 <d7703_30@d7703-30ui-imac.local>
2019-03-07 17:27:51 +0900

    self.view.backgroundColor = UIColor.yellow

--- gitTest02/ViewController.swift
+++ gitTest02/ViewController.swift
@@ -13,13 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.yellow
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
